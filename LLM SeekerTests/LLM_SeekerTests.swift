38251cf58ca43bfc742a2334ac892fec2a2084c5
João Sabino <joasabino@deloitte.pt>
2026-06-14 14:27:22 +0100

    Initial Commit

---
+++ LLM SeekerTests/LLM_SeekerTests.swift
@@ -0,0 +1,18 @@
+//
+//  LLM_SeekerTests.swift
+//  LLM SeekerTests
+//
+//  Created by Sabino, Joao Gabriel on 14/06/2026.
+//
+
+import Testing
+
+struct LLM_SeekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
